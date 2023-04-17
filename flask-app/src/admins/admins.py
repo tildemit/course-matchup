8b9bd4fb7234e7340f1d4dcbdab3afb3e88ac9bf
akash-v888 <76746127+akash-v888@users.noreply.github.com>
2023-04-17 18:30:44 -0400

    added get routes
    added searches to get students with GPAs above or below a given number, added a get route to filter students by year, added a get route to display professors in descending order by their rating

--- flask-app/src/admins/admins.py
+++ flask-app/src/admins/admins.py
@@ -84,3 +84,141 @@ def add_course():
     db.get_db().commit()
 
     return 'Success!'
+
+
+
+# Get all the students at or above a given gpa
+
+
+@admins.route('/getStudentsAboveGPA', methods=['GET'])
+def getStudentsAboveGPA():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # grab the data from the request object
+    the_data = request.json['gpa']
+
+    # build the query string
+    query = "select userID, firstN, lastN from Students natural join Transcripts where GPA >= '" + the_data + "'"
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Get all the students below a given gpa
+
+
+@admins.route('/getStudentsBelowGPA', methods=['GET'])
+def getStudentsBelowGPA():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # grab the data from the request object
+    the_data = request.json['gpa']
+
+    # build the query string
+    query = "select userID, firstN, lastN from Students natural join Transcripts where GPA < '" + str(the_data) + "'"
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Get all the students in a given year
+
+
+@admins.route('/getYearStudents', methods=['GET'])
+def getYearStudents():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # grab the data from the request object
+    the_data = request.json['year']
+
+    # build the query string
+    query = "select userID, firstN, lastN from Students where year = '" + str(the_data) + "'"
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Get all the instructors in descending order of rating
+
+
+@admins.route('/getDescRatingInstruct', methods=['GET'])
+def getDescRatingInstruct():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # build the query string
+    query = "select teacherID, name, rating from Instructors natural join InstructorRatings order by rating DESC"
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
