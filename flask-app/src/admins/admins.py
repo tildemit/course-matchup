017df5a15fa4c9e148f13b516532e47397098e98
Amit Chandra <xenopixelz@gmail.com>
2023-04-17 13:05:15 -0400

    Added getStudents, addCourse, updateName, and addInterest routes + tests

--- flask-app/src/admins/admins.py
+++ flask-app/src/admins/admins.py
@@ -1,6 +1,86 @@
-from flask import Blueprint, request, jsonify, make_response
+from flask import Blueprint, request, jsonify, make_response, current_app
 import json
 from src import db
 
 
 admins = Blueprint('admins', __name__)
+
+# Get all the students in the given department
+
+
+@admins.route('/getStudents', methods=['GET'])
+def get_students():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # grab the data from the request object
+    the_data = request.json['department']
+
+    # build the query string
+    query = "select userID, firstN, lastN from Students natural join Departments where college = '" + the_data + "'"
+
+    # use cursor to query the database for a list of products
+    cursor.execute(query)
+
+    # grab the column headers from the returned data
+    column_headers = [x[0] for x in cursor.description]
+
+    # create an empty dictionary object to use in
+    # putting column headers together with data
+    json_data = []
+
+    # fetch all the data from the cursor
+    theData = cursor.fetchall()
+
+    # for each of the rows, zip the data elements together with
+    # the column headers.
+    for row in theData:
+        json_data.append(dict(zip(column_headers, row)))
+
+    return jsonify(json_data)
+
+
+# Update a professor's name
+
+@admins.route('/updateName', methods=['PUT'])
+def update_name():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # grab the data from the request object
+    teacher_id = request.json['teacher_id']
+    name = request.json['name']
+
+    # build the query string
+    query = "UPDATE Instructors SET name = '" + name + \
+        "' WHERE teacherID = '" + str(teacher_id) + "'"
+
+    # execute and commit
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
+
+
+# Add a new course to the database
+
+@admins.route('/addCourse', methods=['POST'])
+def add_course():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # grab the data from the request object
+    courseID = request.json['courseID']
+    numCredits = request.json['numCredits']
+    name = request.json['name']
+    department_code = request.json['department_code']
+
+    # build the query string
+    query = "INSERT INTO Courses(courseID,numCredits,name,department_code) VALUES ('" + str(
+        courseID) + "', '" + str(numCredits) + "', '" + name + "', '" + str(department_code) + "')"
+
+    # execute and commit
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
--- flask-app/src/students/students.py
+++ flask-app/src/students/students.py
@@ -4,3 +4,28 @@ from src import db
 
 
 students = Blueprint('students', __name__)
+
+# Add a new interest the student has developed
+
+
+@students.route('/addInterest', methods=['POST'])
+def add_interest():
+    # get a cursor object from the database
+    cursor = db.get_db().cursor()
+
+    # grab the data from the request object
+    userID = request.json['userID']
+    academic = request.json['academic']
+    personal = request.json['personal']
+    professional = request.json['professional']
+
+    # build the query string
+    query = "INSERT INTO Interests(userID,academic,personal,professional) VALUES ('" + \
+        str(userID) + "', '" + academic + "', '" + \
+        personal + "', '" + professional + "')"
+
+    # execute and commit
+    cursor.execute(query)
+    db.get_db().commit()
+
+    return 'Success!'
